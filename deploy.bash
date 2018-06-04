56995ee523843855b35b5e4da7097faa16ab9892
Jimmy Sawczuk <github@jimmysawczuk.com>
2018-06-04 18:20:53 -0400

    Adding deploy script

---
+++ deploy.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+APPNAME="gpmdp"
+VERSION="0.1.0"
+
+# Setup
+mkdir -p deploy
+echo "#!/bin/bash" > go-env.sh
+go env >> go-env.sh;
+source go-env.sh;
+
+echo "Host OS:" $GOHOSTOS;
+echo "Host Architecture:" $GOHOSTARCH;
+echo ""
+
+# Build
+for GOOS in windows darwin linux; do
+	for GOARCH in amd64 386; do
+		exe=""
+		if [[ $GOOS == "windows" ]]; then
+			exe=".exe"
+		fi
+
+		echo "$GOOS/$GOARCH" && echo "----------------------------";
+		GOOS=$GOOS GOARCH=$GOARCH go build -v -o $APPNAME github.com/jimmysawczuk/gpmdp
+
+		# if this is the host OS/arch, the exe is put in the root of bin rather than a subdirectory
+		if [[ $GOOS == $GOHOSTOS ]] && [[ $GOARCH == $GOHOSTARCH ]]; then
+			mv $APPNAME deploy/$APPNAME-v$VERSION-${GOOS}-${GOARCH}$exe
+		else
+			mv $APPNAME deploy/$APPNAME-v$VERSION-${GOOS}-${GOARCH}$exe
+		fi
+		echo "";
+	done
+done
+
+# Cleanup
+rm go-env.sh
